5013e3e6e90de96825cc30b621df752cae2b0d33
Valentina Assef <valenassef@gmail.com>
2023-03-28 11:54:27 -0300

    Carga de data

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,33 @@
 const { createApp } = Vue //Desestructuring: Rescatamos el createApp de Vue
 
-createApp({
+const app = createApp({
 
   data() {
-    return {
-      message: 'Hello Vue!', //Propiedades reactivas de Vue. Cambia el valor en todos los lugares que se use sin necesidad que yo le indique donde cambiarlo
+    return { //Propiedades reactivas de Vue. Cambia el valor en todos los lugares que se use sin necesidad que yo le indique donde cambiarlo
+      apiLocation: "../data/amazing.json",
+      events: [],
     }
   },
 
-  created(){
-     this.message = 'Hola Valen!!' //con this le pido que me traiga el message de data
+  created(){ //con this le pido que me traiga el message de data
+     this.getData 
   },
 
   mounted(){
+
+  },
+
+  methods:{
+    getData(){
+      fetch(this.apiLocation)
+        .then(response => response.json())
+        .then(dataApi => {
+          this.events = dataApi
+        })
+    }
+  },
+
+  computed:{
 
   }
 
