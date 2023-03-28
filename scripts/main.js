c0f544aee84245739a92cbfa82ffcb467481b60f
Valentina Assef <valenassef@gmail.com>
2023-03-28 20:50:55 -0300

    Filtrados

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,14 @@
 const { createApp } = Vue //Desestructuring: Rescatamos el createApp de Vue
 
 const app = createApp({
-  data() {
-    return { //Propiedades reactivas de Vue. Cambia el valor en todos los lugares que se use sin necesidad que yo le indique donde cambiarlo
+  data() { //Propiedades reactivas de Vue. Cambia el valor en todos los lugares que se use sin necesidad que yo le indique donde cambiarlo
+    return { 
       apiUrl: '../data/amazing.json',
       events: [],
+      backupEvents: [],
+      text: '',
+      categories: [],
+      selectedCategories: [],
     }
   },
 
@@ -23,13 +27,46 @@ const app = createApp({
         .then(response => response.json())
         .then(dataApi => {
             this.events = dataApi.events
+            this.backupEvents = this.events
+            this.categoriesList(this.events)
         })
+        .catch(error => console.log(error.message))
+    },
+
+    categoriesList(array){
+      array.forEach(element =>{
+        if(!this.categories.includes(element.category)){
+          this.categories.push(element.category)
+        }
+      })
     }
+
   },
 
   computed:{
+    combinedFilter(){
+      let firstFilter = this.backupEvents.filter(evento => evento.name.toLowerCase().includes(this.text.toLowerCase()));
+      if(!this.selectedCategories.length){
+        this.events = firstFilter
+      } else {
+        this.events = firstFilter.filter(evento => this.selectedCategories.includes(evento.category))
+      }
+    }
 
-  }
+    //NOTA: Como hubiesen quedado los filtros independientes
+    /* filterByText(){
+        //this.text = document.querySelector('input').value //Ya no es necesario definirlo porque le agregue el v-model al input del html
+      this.events = this.backupEvents.filter(evento => evento.name.toLowerCase().includes(this.text.toLowerCase()));
+    },
+
+    filterByCategory(){
+      if(!this.selectedCategories.length){
+        this.events = this.backupEvents
+      } else {
+        this.events = this.backupEvents.filter(evento => this.selectedCategories.includes(evento.category))
+      }
+    } */
+  },
 
   
 }).mount('#app') //Metodo que se usara en el contenedor con id:app
